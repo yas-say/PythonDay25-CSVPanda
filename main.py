1104189158054fa34d7ecce0152a1b28e7fdc168
yas-say <yasowant@live.com>
2023-03-19 22:07:46 -0400

    initial intro to panda

--- main.py
+++ main.py
@@ -3,18 +3,23 @@
 #
 # print(data)
 
-import csv
-
-temperature = []
-with open("weather_data.csv") as file:
-    data = csv.reader(file)
-    for row in data:
-        if row[1] != "temp":
-            temperature.append(int(row[1]))
-
-# temperature.pop(0)
+# import csv
 #
-# for i in range(0,len(temperature)):
-#     temperature[i] = int(temperature[i])
+# temperature = []
+# with open("weather_data.csv") as file:
+#     data = csv.reader(file)
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#
+# # temperature.pop(0)
+# #
+# # for i in range(0,len(temperature)):
+# #     temperature[i] = int(temperature[i])
+#
+# print(temperature)
 
-print(temperature)
+import pandas
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
+
