ecd064d905a4af392b4ba92a9c9d7c7fa1a1e611
yas-say <yasowant@live.com>
2023-03-19 22:28:06 -0400

    some more panda examples

--- main.py
+++ main.py
@@ -21,7 +21,7 @@
 
 import pandas
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+print(data)
 
 
 data_list = data["temp"].to_list()
@@ -33,7 +33,17 @@ print(data_list)
 #     sum += _
 #     ctr += 1
 #
-# print(f"Avergae is {sum/ctr}")
+# # print(f"Avergae is {sum/ctr}")
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# # Get Row
+# print(data[data.day == "Monday"])
+#
+# print(data[data.temp == data.temp.max()])
 
-print(data["temp"].mean())
-print(data["temp"].max())
+
+monday = data[data.day == "Monday"]
+print(monday.temp)
+print(f"Temp in f: {(int(monday.temp) * 1.8) + 32}")
