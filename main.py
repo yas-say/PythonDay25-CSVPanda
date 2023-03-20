71b594e8a7009e7559835184802f8b1f53fcccf4
yas-say <yasowant@live.com>
2023-03-19 22:04:17 -0400

    int temp pulled

--- main.py
+++ main.py
@@ -3,3 +3,18 @@
 #
 # print(data)
 
+import csv
+
+temperature = []
+with open("weather_data.csv") as file:
+    data = csv.reader(file)
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+
+# temperature.pop(0)
+#
+# for i in range(0,len(temperature)):
+#     temperature[i] = int(temperature[i])
+
+print(temperature)
